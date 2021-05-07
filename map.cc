03418609827da9ec593e1c85933a03e1963ae4dc
Rishi Patel <rtpatel2@siebl-0224-12.ews.illinois.edu>
2021-05-07 18:43:02 -0500

    Clean up documentation in Map class

--- map.cc
+++ map.cc
@@ -5,14 +5,64 @@
 #include <set>
 #include <utility>
 #include <iostream>
-typedef std::unordered_map<std::string, std::unordered_map<std::string, double>> FlightGraph;
 
+Map::Map(const cs225::PNG& map): map_(map) {}
 
-Map::Map(cs225::PNG& map): map(map) { }
+void Map::AddPoint(double lat, double lng, double luminance) {
+    std::pair<double, double> projection = ProjectMercator(lat, lng);
+    double x = projection.first;
+    double y = projection.second;
+
+    cs225::HSLAPixel pixel_color(265, 1, luminance, 1);
+    for (int i = max(0, (int) x - 1); i < min((int) map_.width(), (int) x + 1); ++i) {
+        for (int j = max(0, (int) y - 1); j < min((int) map_.height(), (int) y + 1); ++j) {
+            map_.getPixel(i,j) = pixel_color;  
+        }
+    }
+}
+
+void Map::Animate(const FlightGraph& graph,  AirportMap& airports, const std::string& origin, const std::string& gif_filename, const std::string& png_filename){
+    Animation animation;
+    cs225::PNG temp = cs225::PNG(map_);
+    animation.addFrame(temp);
+
+    int last_seen_degree = -1;
+
+    std::queue<std::pair<std::string, int>> queue;
+    std::set<std::string> visited;
+    visited.insert(origin);
+    queue.emplace(origin, 0);
+
+    while (!queue.empty()) {
+        std::string curr = queue.front().first;
+        int curr_degree = queue.front().second;
+
+        AddPoint(airports.at(curr).getLatitude(), airports.at(curr).getLongitude(), fmin(0.25 + (last_seen_degree + 1) * 0.15, 1.0));
+
+        if (curr_degree != last_seen_degree) {
+            last_seen_degree = curr_degree;
+            cs225::PNG temp = cs225::PNG(map_);
+            animation.addFrame(temp);
+        }   
+        queue.pop();
+
+        for (auto& it: graph.at(curr)) {
+            if (visited.count(it.first) == 0) {
+                queue.emplace(it.first, curr_degree + 1);
+                visited.insert(it.first);
+            }
+        }
+    }
+
+    animation.write(gif_filename);
+
+    cs225::PNG lastFrame = animation.getFrame(animation.frameCount() - 1);
+ 	lastFrame.writeToFile(png_filename);
+}
 
 std::pair<double, double> Map::ProjectMercator(double lat, double lng) {
-    double width = map.width();
-    double height = map.height();
+    double width = map_.width();
+    double height = map_.height();
 
     // formula derived from:
     // https://stackoverflow.com/questions/14329691/convert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection
@@ -26,65 +76,6 @@ std::pair<double, double> Map::ProjectMercator(double lat, double lng) {
     return std::make_pair(x, y);
 }
 
-void Map::AddPoint(double lat, double lng, double luminance) {
-    std::pair<double, double> projection = ProjectMercator(lat, lng);
-
-    double x = projection.first;
-    double y = projection.second;
-
-    // cs225::HSLAPixel pixel_color(0, 1, 0.5, 1);
-    cs225::HSLAPixel pixel_color(265, 1, luminance, 1);
-
-    for(int i = std::max(0, (int) x - 1); i < std::min((int) map.width(), (int) x + 1); i++) {
-        for(int j = std::max(0, (int) y - 1); j < std::min((int) map.height(), (int) y + 1); j++) {
-              map.getPixel(i,j) = pixel_color;  
-        }
-    }
-}
-
-
-void Map::Animate(const FlightGraph& graph,  AirportList& airports, const std::string& origin, const std::string& gif_filename, const std::string& png_filename){
-    Animation animation;
-    cs225::PNG temp = cs225::PNG(map);
-    animation.addFrame(temp);
-
-    int last_seen_degree = -1;
-    double current_lum = 0;
-
-    std::queue<std::pair<std::string, int>> queue;
-    std::set<std::string> visited;
-    visited.insert(origin);
-    queue.emplace(origin, 0);
-
-    while(!queue.empty()) {
-        std::string curr = queue.front().first;
-        int curr_degree = queue.front().second;
-
-        AddPoint(airports.at(curr).getLatitude(), airports.at(curr).getLongitude(), fmin(0.25 + (last_seen_degree + 1) * 0.15, 1.0));
-
-        if(curr_degree != last_seen_degree) {
-            last_seen_degree = curr_degree;
-            cs225::PNG temp = cs225::PNG(map);
-            animation.addFrame(temp);
-        }   
-
-        queue.pop();
-
-        for(auto& it: graph.at(curr)) {
-            if(visited.count(it.first)==0) {
-                queue.emplace(it.first, curr_degree + 1);
-                visited.insert(it.first);
-            }
-        }
-
-    }
-
-    animation.write(gif_filename);
-
-    cs225::PNG lastFrame = animation.getFrame(animation.frameCount() - 1);
- 	lastFrame.writeToFile(png_filename);
-}
-
 cs225::PNG& Map::GetMap() {
-    return map;
+    return map_;
 }
--- map.h
+++ map.h
@@ -1,18 +1,48 @@
 #pragma once
+
 #include "cs225/PNG.h"
 #include "network.h"
 #include "cs225/Animation.h"
 #include <string>
-typedef const std::unordered_map<std::string, Airport> AirportList;
 
+typedef std::unordered_map<std::string, Airport> AirportMap;
+typedef std::unordered_map<std::string, std::unordered_map<std::string, double>> FlightGraph;
+
+/** Maintains functionality of a Mercator projection of a globe. */
 class Map {
-    public:
-        Map(cs225::PNG& map);
-        void AddPoint(double lat, double lng, double luminance);
-        cs225::PNG& GetMap();
-        void Animate(const FlightGraph& graph,  AirportList& airports, const std::string& origin, const std::string& gif_filename, const std::string& png_filename);
-        std::pair<double, double> ProjectMercator(double lat, double lng);
+  public:
+    /** 
+     * Creates a map with the given PNG background.
+     * @param map PNG background
+     */
+    Map(const cs225::PNG& map);
+    /** 
+     * Adds a point to this map.
+     * @param lat latitude to add to the map
+     * @param lng longitude to add to the map
+     * @param luminance luminance of pixels modified by adding a point
+     */
+    void AddPoint(double lat, double lng, double luminance);
+    /** 
+     * Creates an animation of a BFS.
+     * @param graph an undirected graph of flights between airports
+     * @param airports a map between 3-letter airport codes and airport objects
+     * @param origin 3-letter airport code of the starting point of the BFS
+     * @param gif_filename filename to which to save GIF animation of BFS
+     * @param png_filename filename to which to save last frame of BFS animation
+     */
+    void Animate(const FlightGraph& graph, AirportMap& airports, const std::string& origin, const std::string& gif_filename, const std::string& png_filename);
+    
+    /**
+     * Projects a given latitude and longitude to coordinates on a mercator projection.
+     * @param lat latitude
+     * @param lng longitude
+     * @return a pair of (x, y) representing the coordinates on the projection
+     */
+    std::pair<double, double> ProjectMercator(double lat, double lng);
+    
+    cs225::PNG& GetMap();
 
-    private:
-        cs225::PNG map;
+  private:
+    cs225::PNG map_;
 };
